6a4cc486f2fb87b52f16a91acb916c6470b46861
Markus Flo <markusflohj@gmail.com>
2025-11-15 06:41:23 +0100

    alle tester fungerer

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -5,11 +5,29 @@ import java.io.PrintWriter;
 
 import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave3.*;
+import no.hvl.dat100.oppgave1.Innlegg;
 
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		try {
+			PrintWriter writer = new PrintWriter(mappe + filnavn);
+
+			writer.println(samling.getAntall());
+
+			for (Innlegg innlegg : samling.getSamling()) {
+				if (innlegg != null) {
+					writer.print(innlegg.toString());
+				}
+			}
+
+			writer.close();
+
+			return true;
+		} catch (FileNotFoundException e) {
+			System.out.println("Fil ikke funnet: " + e.getMessage());
+			return false;
+		}
 	}
 }
